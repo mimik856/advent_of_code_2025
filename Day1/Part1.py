90d76b12ee055b6260c81452c31de7a106c6d775
mimik856 <148219315+mimik856@users.noreply.github.com>
2025-12-05 14:50:28 +0100

    Create Part1.py

---
+++ Day1/Part1.py
@@ -0,0 +1,20 @@
+def read():
+    with open("FILE_NAME.txt") as f:
+        return [ln.strip() for ln in f if ln.strip()]
+
+dial = 50
+count = 0
+
+for line in read():
+    direction = line[0]
+    n = int(line[1:])
+
+    if direction == "L":
+        n = -n
+
+    dial = (dial + n) % 100
+
+    if dial == 0:
+        count += 1
+
+print(f"Final password: {count}")
